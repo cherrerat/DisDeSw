5cb138656f33ce890c52c0a21a5aacf930d00dec
cherrera <chudikj@live.com>
2017-11-20 16:57:52 -0300

    Mapas fixed soy un D  1 0 S  A C M 1 P T

--- public/js/CabPas.js
+++ public/js/CabPas.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
         $("#datosBusq").hide();
         $("#map").show();
         $("#map").css('height','800px');
+        var aux=document.getElementById('viaje_id').value;
+        var newPos=aux.split(",");
+        var lat = newPos[0].split("{lat: ");
+        var lng = newPos[1].split("lng: ");
+        lng[1] = lng[1].replace("}","");
+        newLatLng = new google.maps.LatLng(lat[1],lng[1]);
+        crearMapa(newLatLng);
     });
 });
